613845fcdc800e038132513811297f1f2e922c67
Chris Ragan <ragan.christopher.a@gmail.com>
2023-08-01 08:07:25 -0400

    removed tests for demo

--- demo-console-app/Program.cs
+++ demo-console-app/Program.cs
@@ -3,6 +3,6 @@ using UnitDemo.Library.Services;
 
 // See https://aka.ms/new-console-template for more information
 var fac = new BikeFactory();
-var bike = fac.GetBikeOfType((BikeType)5);
+var bike = fac.GetBikeOfType(BikeType.Mountain);
 
 Console.WriteLine($"Your bike is a {bike.Description} bike, with {bike.Handlebars} bars and {bike.Tires} tires.");
